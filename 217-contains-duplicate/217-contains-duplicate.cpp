77b79a9db60bcaa3c98289b28931f4bb3034d042
Md Fakrul Islam Taraque <63301304+MdFakrulIslamTaraque@users.noreply.github.com>
2022-08-23 11:49:22 +0600

    Time: 176 ms (29.47%), Space: 46.5 MB (92.05%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int,int>m1;
+        // map<int,int>m1;
         int num_size = nums.size();
-        if(num_size > 1)
+        // if(num_size > 1)
+        // {
+        //     m1[nums[0]] = 1;
+        //     for(int i=1; i<num_size; i++)
+        //     {
+        //         auto it = m1.find(nums[i]);
+        //         if(it != m1.end())
+        //         {
+        //             return true;
+        //         }
+        //         m1[nums[i]] = 1;
+        //     }
+        //     return false;
+        // }
+        // return false;
+        if(num_size>1)
         {
-            m1[nums[0]] = 1;
+            sort(nums.begin(),nums.end());
             for(int i=1; i<num_size; i++)
             {
-                auto it = m1.find(nums[i]);
-                if(it != m1.end())
-                {
+                if(nums[i] == nums[i-1])
                     return true;
-                }
-                m1[nums[i]] = 1;
             }
             return false;
         }
