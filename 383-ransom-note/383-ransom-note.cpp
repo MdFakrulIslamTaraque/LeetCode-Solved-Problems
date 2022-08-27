70a63e98919b3e615b36530c143ee04095f0ec3a
Md Fakrul Islam Taraque <63301304+MdFakrulIslamTaraque@users.noreply.github.com>
2022-08-27 16:22:43 +0600

    Time: 46 ms (18.57%), Space: 9 MB (15.41%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>fr,fm;
+        int lr = ransomNote.size(), lm = magazine.size();
+        for(int i=0; i<lr; i++) fr[ransomNote[i]]++;
+        for(int i=0; i<lm; i++) fm[magazine[i]]++;
+        char cr;
+        
+        //if not found in magazine
+        for(auto i:fr)
+        {
+            cr = i.first;
+            if(fm.find(cr) == fm.end())
+                return false;
+        }
+        
+        //checking frequency, if greater, false
+        for(auto i:fr)
+        {
+            cr = i.first;
+            if(fr[cr] > fm[cr])
+                return false;
+        }
+        
+        return true;
+        
+    }
+};
