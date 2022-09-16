a2488c3bb945910046a5811a70bb9c703c120bc7
Md Fakrul Islam Taraque <63301304+MdFakrulIslamTaraque@users.noreply.github.com>
2022-09-16 12:21:17 +0600

    Time: 13 ms (5.73%), Space: 8.5 MB (37.10%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        int s = nums.size();
-        vector<int>ans(s);
-        ans[0] = nums[0];
+         int s = nums.size();
+        // vector<int>ans(s);
+        // ans[0] = nums[0];
         for(int i=1; i<s; i++)
         {
-            ans[i] = ans[i-1] + nums[i];
+            nums[i] = nums[i-1] + nums[i];
         }
-        return ans;
+        return nums;
     }
 };
