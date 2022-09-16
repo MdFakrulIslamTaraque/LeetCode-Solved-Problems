60f6c9b5baa4733e184cdc95d44bfc59e6e3ccd8
Md Fakrul Islam Taraque <63301304+MdFakrulIslamTaraque@users.noreply.github.com>
2022-09-16 13:19:01 +0600

    Time: 0 ms (100.00%), Space: 5.8 MB (78.69%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int fib(int n) {
-        // vector<int>fibV;
-        // fibV.push_back(0);        
-        // fibV.push_back(1);
         if(n == 0) return 0;
         if(n == 1) return 1;
         int v0 = 0, v1 = 1, now;
